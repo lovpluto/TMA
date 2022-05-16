9206ace72d2ac0917c6ee214dd983ee2fae0c27b
Jack <tiendatmema@gmail.com>
2022-05-16 16:25:59 +0700

    abc

---
+++ index.js
@@ -0,0 +1,47 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('bm'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/fullAnimateForWeb.json' // the path to the animation json
+  });
+var animation1 = lottie.loadAnimation({
+    container: document.getElementById('logoOne'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/tmaLogoFirst.json' // the path to the animation json
+  });
+var animation2 = lottie.loadAnimation({
+    container: document.getElementById('logoTwo'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/tmaLogoSecond.json' // the path to the animation json
+  });
+var animation3 = lottie.loadAnimation({
+    container: document.getElementById('logoThree'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/tmaLogoThird.json' // the path to the animation json
+  });
+var animation4 = lottie.loadAnimation({
+    container: document.getElementById('logoFour'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/tmaLogoFour.json' // the path to the animation json
+  });
+var animation5 = lottie.loadAnimation({
+    container: document.getElementById('logoFive'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../export/tmaLogoFive.json' // the path to the animation json
+  });
+
+
+setInterval(() => {
+  animation,animation1,animation2,animation3,animation4,animation5
+}, 500);
