3980868985690b6acd9a307b2691afe00c737477
Jack <tiendatmema@gmail.com>
2022-05-16 16:31:03 +0700

    abc

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ var animation = lottie.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/fullAnimateForWeb.json' // the path to the animation json
+    path: './json/fullAnimateForWeb.json' // the path to the animation json
   });
 var animation1 = lottie.loadAnimation({
     container: document.getElementById('logoOne'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/tmaLogoFirst.json' // the path to the animation json
+    path: './json/tmaLogoFirst.json' // the path to the animation json
   });
 var animation2 = lottie.loadAnimation({
     container: document.getElementById('logoTwo'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/tmaLogoSecond.json' // the path to the animation json
+    path: './json/tmaLogoSecond.json' // the path to the animation json
   });
 var animation3 = lottie.loadAnimation({
     container: document.getElementById('logoThree'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/tmaLogoThird.json' // the path to the animation json
+    path: './json/tmaLogoThird.json' // the path to the animation json
   });
 var animation4 = lottie.loadAnimation({
     container: document.getElementById('logoFour'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/tmaLogoFour.json' // the path to the animation json
+    path: './json/tmaLogoFour.json' // the path to the animation json
   });
 var animation5 = lottie.loadAnimation({
     container: document.getElementById('logoFive'), // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '../export/tmaLogoFive.json' // the path to the animation json
+    path: './json/tmaLogoFive.json' // the path to the animation json
   });
 
 
